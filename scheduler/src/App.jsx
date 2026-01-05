02b386aaba79134d9870db580517e52abf01a1ae
Dalibor Jovanovski <dalibor.jovanov2k1@gmail.com>
2026-01-05 22:45:03 +0100

    added mobile responsiveness

--- scheduler/src/App.jsx
+++ scheduler/src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import Header from "./components/layout/Header.jsx";
 import DayPlanner from "./components/layout/DayPlanner.jsx";
 import { TaskContextProvider } from "./store/TaskContext.jsx";
@@ -7,14 +9,27 @@ import ViewTask from "./components/layout/ViewTask.jsx";
 import EditTask from "./components/layout/EditTask.jsx";
 import Calendar from "./components/layout/Calendar.jsx";
 import Footer from "./components/UI/Footer.jsx";
+import MenuBar from "./components/UI/MenuBar.jsx";
 
 function App() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  function toggleMenu() {
+    setIsMenuOpen((prev) => !prev);
+  }
+
+  function closeMenu() {
+    setIsMenuOpen(false);
+  }
+
   return (
     <TaskContextProvider>
       <ModalContextProvider>
         <div className="flex flex-col min-h-screen">
-          <Header />
-          <main className="flex-1">
+          <Header onMenuToggle={toggleMenu} isMenuOpen={isMenuOpen} />
+          <MenuBar isOpen={isMenuOpen} onClose={closeMenu} />
+
+          <main className="flex-1 w-full p-3 md:p-6">
             <AddTaskForm />
             <DayPlanner />
             <ViewTask />
